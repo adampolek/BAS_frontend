454f06b5767c0d2416ba0dc0f6c22d59d9da607b
adam <adampolejczuk@gmail.com>
2021-04-11 17:18:47 +0200

    Add input password visibilty toggle component and start account view

--- frontend/src/components/Input.js
+++ frontend/src/components/Input.js
@@ -1,7 +1,15 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import './css/input.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+
+const eye = <FontAwesomeIcon icon={faEye} />;
 
 const Input = ({ caption = 'Holder', error='Error', showError=false, value, type='text', width="250px", onChange, ...props }) => {
+    const [passwordShown, setPasswordShown] = useState(type =="password" ? false : true);
+    const togglePasswordVisiblity = () => {
+        setPasswordShown(passwordShown ? false : true);
+      };
     // focus input element on label click
     const useFocus = () => {
         const htmlElRef = useRef(null)
@@ -13,9 +21,10 @@ const Input = ({ caption = 'Holder', error='Error', showError=false, value, type
     return (
         <div style={{margin:'5px'}}>
         <div className='group'>
-            <input style={{width: width}} id='value' type={type} className={(showError ? 'input_error' : '') + ' input'} 
+            <input style={{width: width}} id='value' type={passwordShown ? "text" : "password"} className={(showError ? 'input_error' : '') + ' input'} 
                 placeholder='text' value={value} onChange={onChange} {...props} ref={inputRef}/>
             <label className={(showError ? 'label_error' : '') + ' label'} onClick={setInputFocus} >{caption}</label>
+            <i onClick={togglePasswordVisiblity} style={type != "password" ? {visibility: "hidden"} : {visibility: "visible"}}>{eye}</i>
         </div>
         <label hidden={!showError} className='error'>{error}</label>
         </div>
--- frontend/src/views/Account.js
+++ frontend/src/views/Account.js
@@ -1,9 +1,75 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Button from '../components/Button';
+import Col from '../components/Col';
+import Input from '../components/Input';
+import Panel from '../components/Panel';
+import Row from '../components/Row';
+import DatePicker from 'react-date-picker';
+
 
 const Account = (props) => {
+
+    const [selectedOption, setSelectedOption] = useState('');
+    const [value, onChange] = useState(new Date());
+
     return (
-        <div>
-            <h1>Account</h1>
+        <div style={{height: "100%", width: "100%", position: "absolute", backgroundColor: "purple"}}>
+            <Panel width={"100%"} height={"100%"}>
+                <Col>
+                    <h3 style={{fontWeight: "bold"}}>Personal information</h3>
+                    <div style={{backgroundColor: "#E0E0E0", height: '2px', width: '100%'}}></div>
+                    <Row>
+                        <Input caption='Name' width="300px" />
+                        <Input caption='Full Name' width="300px" />
+                        <Input caption='Height' width="300px" />
+                    </Row>
+                    <Row>
+                        <div className="radio">
+                            <label>
+                                <input type="radio" value="male" checked={selectedOption === 'male'} onClick={(e) => setSelectedOption(e.target.value)} />
+                                Male
+                            </label>
+                        </div>
+                        <div className="radio">
+                            <label>
+                                <input type="radio" value="female" checked={selectedOption === 'female'} onClick={(e) => setSelectedOption(e.target.value)} />
+                                Female
+                            </label>
+                        </div>
+                    </Row>
+                    <DatePicker
+                        onChange={onChange}
+                        value={value}
+                    />
+
+                    <Row>
+                        <Button value="Save"/> 
+                    </Row>
+
+
+                    <h3 style={{fontWeight: "bold"}}>E-mail address</h3>
+                    <div style={{backgroundColor: "#E0E0E0", height: '2px', width: '100%'}}></div>
+                    <Row>
+                            <Input caption='E-mail address' width="300px" />
+                            <Input caption='Password' width="300px" type="password"/>
+                    </Row>
+                    <Row>
+                        <Button value="Save" />
+                    </Row>
+
+
+                    <h3 style={{fontWeight: "bold"}}>Password</h3>
+                    <div style={{backgroundColor: "#E0E0E0", height: '2px', width: '100%'}}></div>
+                    <Row>
+                        <Input caption='Current password' width="300px" type="password"/>
+                        <Input caption='New password' width="300px" type="password"/>
+                        <Input caption='Confirm password' width="300px" type="password"/>
+                    </Row>
+                    <Row>
+                        <Button value="Save" />
+                    </Row>
+                </Col>
+            </Panel>
         </div>
     )
 }
