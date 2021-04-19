4cd49ebdda8706f8bac97429c29f8ab964dcb265
adam <adampolejczuk@gmail.com>
2021-04-19 18:01:00 +0200

    added too input in daily

--- frontend/src/views/DailyDiagnosis.js
+++ frontend/src/views/DailyDiagnosis.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import JAMCol from '../components/JAMCol';
 import JAMInput from '../components/JAMInput';
 import JAMLabel from '../components/JAMLabel';
@@ -22,27 +22,34 @@ const DailyDiagnosis = (props) => {
             insulin: parseInt(insulin),
             bloodPressure: parseInt(bloodPressure),
             entryDate: new Date()
-        }, {headers: {Authorization: JSON.parse(localStorage.getItem('token'))}}).then(res => {
+        }, { headers: { Authorization: JSON.parse(localStorage.getItem('token')) } }).then(res => {
             console.log(res);
         })
     };
 
     return (
-        <div style={{height: "100%", width: "100%", position: "absolute", backgroundColor: "purple"}}>
+        <div style={{ height: "100%", width: "100%", position: "absolute", backgroundColor: "purple" }}>
             <JAMPanel width={"100%"} height={"100%"}>
-                <Menubar/>
+                <Menubar />
                 <JAMCol>
-                    <JAMLabel caption='Fill up your daily diagnosis' header/>
-                    <JAMLine style={{marginTop: '20px', marginBottom: '20px'}}/>
+                    <JAMLabel caption='Fill up your daily diagnosis' header />
+                    <JAMLine style={{ marginTop: '20px', marginBottom: '20px' }} />
                     <JAMInput type='number' caption='Weight' value={weight}
-                              onChange={(e) => setWeight(e.target.value)}/>
+                        onChange={(e) => setWeight(e.target.value)} />
                     <JAMInput type='number' caption='Glucose' value={glucose}
-                              onChange={(e) => setGlucose(e.target.value)}/>
+                        onChange={(e) => setGlucose(e.target.value)} />
                     <JAMInput type='number' caption='Insulin' value={insulin}
-                              onChange={(e) => setInsulin(e.target.value)}/>
+                        onChange={(e) => setInsulin(e.target.value)} />
                     <JAMInput type='number' caption='Blood Pressure' value={bloodPressure}
-                              onChange={(e) => setBloodPressure(e.target.value)}/>
-                    <JAMButton value='Add entry' width={"100%"} onClick={() => addEntry()}/>
+                        onChange={(e) => setBloodPressure(e.target.value)} />
+                    <JAMLabel caption='Additional information' big style={{ marginTop: '20px' }} />
+                    <JAMLine style={{ marginTop: '20px', marginBottom: '20px' }} />
+                    <JAMInput type='number' caption='Amount of cigarettes' />
+                    <JAMInput type='number' caption='Hours of sleep' />
+                    <JAMInput type='number' caption='Glasses of water' />
+                    <JAMInput type='number' caption='Training hours' />
+                    <JAMInput type='number' caption='Amount of alcohol' />
+                    <JAMButton value='Add entry' width={"100%"} onClick={() => addEntry()} />
                 </JAMCol>
             </JAMPanel>
         </div>
