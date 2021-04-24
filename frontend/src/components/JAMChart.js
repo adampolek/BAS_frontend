bc81f16a866ee8f544c9efc88a1b1d4978a5a1a3
mazsak <mazsak97@gmial.com>
2021-04-24 13:07:54 +0200

    added bar chart

--- frontend/src/components/JAMChart.js
+++ frontend/src/components/JAMChart.js
@@ -5,13 +5,14 @@ import JAMPanel from './JAMPanel';
 import JAMRow from './JAMRow';
 
 const JAMChart = ({ title = 'Title', caption = 'Name data', type = 'line', labels = [], data = [], color = 'purple', hintColor = '#E0E0E0', borderWidth = 1, fontSize = '60%', width = '100%', height = '100%', numberOfLines = 10, ...props }) => {
-    const canvasRef = useRef(null);
+
     const xPadding = 80 / (labels.length - 1);
     const max = (Math.ceil((Math.max(...data) / 10)) * 10);
     const yPadding = 80 / max;
     const yLinePadding = 80 / numberOfLines;
 
     const line = [];
+    const bars = [];
     const grid = [];
     const points = [];
     const labelsText = [];
@@ -26,18 +27,27 @@ const JAMChart = ({ title = 'Title', caption = 'Name data', type = 'line', label
 
     // tu podpisy do osi y
     for (var i = 0; i < labels.length; i++) {
-        labelsText.push(<tspan y='87%' dx='9%' dy='0' x={(80 / (labels.length - 1) * i) + '%'} style={{ fontSize: fontSize }}>{labels[i]}</tspan>);
+        labelsText.push(<tspan y='87%' dx='9%' dy='0' x={(70 / (labels.length - 1) * i) + '%'} style={{ fontSize: fontSize }}>{labels[i]}</tspan>);
     }
-
-    var previous = { x: '10%', y: (80 - (yPadding * data[0])) + '%' };
-
-    points.push(<circle cx={previous.x} cy={previous.y} r="0.5%" stroke="black" stroke-width="0.5" fill="white" />);
-    grid.push(<line x1='10%' y1='0%' x2='10%' y2='80%' stroke={hintColor} />);
+    
+    
+    if (type === 'line') {
+        var previous = { x: '10%', y: (80 - (yPadding * data[0])) + '%' };
+        points.push(<circle cx={previous.x} cy={previous.y} r="0.5%" stroke="black" stroke-width="0.5" fill="white" />);
+        grid.push(<line x1='10%' y1='0%' x2='10%' y2='80%' stroke={hintColor} />);
+    }else{
+        var previous = { x: '15%', y: (80 - (yPadding * data[0])) + '%' };
+    }
     for (var i = 1; i < data.length; i++) {
-        var current = { x: 10+(xPadding * i) + '%', y: (80 - (yPadding * data[i])) + '%' };
-        points.push(<circle cx={current.x} cy={current.y} r="0.5%" stroke="black" stroke-width="0.5" fill="white" />);
-        line.push(<line x1={previous.x} y1={previous.y} x2={current.x} y2={current.y} stroke={color} />);
-        grid.push(<line x1={10+(xPadding * i) + '%'} y1='0%' x2={10+(xPadding * i) + '%'} y2='80%' stroke={hintColor} />);
+        if (type === 'line') {
+            var current = { x: 10 + (xPadding * i) + '%', y: (80 - (yPadding * data[i])) + '%' };
+            points.push(<circle cx={current.x} cy={current.y} r="0.5%" stroke="black" stroke-width="0.5" fill="white" />);
+            line.push(<line x1={previous.x} y1={previous.y} x2={current.x} y2={current.y} stroke={color} />);
+            grid.push(<line x1={10 + (xPadding * i) + '%'} y1='0%' x2={10 + (xPadding * i) + '%'} y2='80%' stroke={hintColor} />);
+        } else {
+            var current = { x: 15 + (xPadding * i) + '%', y: (80 - (yPadding * data[i])) + '%' };
+            bars.push(<line x1={previous.x} y1={previous.y} x2={previous.x} y2='80%'  stroke={color} stroke-width={xPadding - 5 + '%'} />);
+        }
         previous = current;
     }
 
@@ -50,16 +60,17 @@ const JAMChart = ({ title = 'Title', caption = 'Name data', type = 'line', label
                 </JAMRow>
                 <JAMRow width='100%'>
                     <JAMCol width='100%'>
-                        <svg width={width} height={height}>
+                        <svg width='100%' height={height}>
                             <text>
                                 {valuesText}
                             </text>
                             {grid}
                             {line}
+                            {bars}
                             {points}
                             <text>
-                            {labelsText}
-                        </text>
+                                {labelsText}
+                            </text>
                         </svg>
                     </JAMCol>
                 </JAMRow>
