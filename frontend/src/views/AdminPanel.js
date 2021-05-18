bc7affe2639749022f0e25b4a8cec9551f939666
adam <adampolejczuk@gmail.com>
2021-05-18 20:00:19 +0200

    Add admin panel csv reading

--- frontend/src/views/AdminPanel.js
+++ frontend/src/views/AdminPanel.js
@@ -1,17 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import JAMCombobox from '../components/JAMCombobox';
 import JAMCol from '../components/JAMCol';
 import JAMPanel from '../components/JAMPanel';
 import Menubar from './Menubar';
-import JAMInput from '../components/JAMInput';
-import JAMCheckbox from '../components/JAMCheckbox';
 import JAMRow from '../components/JAMRow';
 import JAMButton from '../components/JAMButton';
 import JAMLoader from '../components/JAMLoader';
+import DataTable from 'react-data-table-component';
 
 const AdminPanel = (props) => {
     
     const [selectedOption, setSelectedOption] = useState("");
+    // TODO: podpiac date
+    const [data, setData] = useState([]);
+    const [columns, setColumns] = useState([]);
+    const cols = (["Gender","Glucose","BloodPressure","Insulin","BMI","Age","Outcome"]);
+
+    useEffect(() => {
+        const columns = cols.map(c => ({
+            name: c,
+            selector: c,
+          }));
+    
+        setColumns(columns);
+    });
+
 
     return (
         <JAMPanel minHeight='700px' width={"90%"} maxWidth={"1300px"} backgroundColor={"white"} minWidth='400px' style={{ marginTop: '70px', marginBottom: '50px', paddingBottom: '50px', paddingTop: '50px' }}>
@@ -20,7 +33,7 @@ const AdminPanel = (props) => {
                 <JAMRow>
                     <JAMCombobox  
                         caption="Learning Method"
-                        // ŻY ŻY TODO: podpiąć
+                        // ŻY ŻY TODO: podpiąć metody uczenia
                         elements={["elem1", "elem2", "elem3"]}
                         value={selectedOption}
                         onChange={(e) => {
@@ -29,14 +42,16 @@ const AdminPanel = (props) => {
                         }} />
                 </JAMRow>
                 <JAMRow>
-                    {/* ŻY ŻY TODO: podpiąć */}
-                    <JAMButton value="Save" />
-                </JAMRow>
-                <JAMLoader />
-                <JAMRow>
+                    {/* ŻY ŻY TODO: podpiąć start uczenia*/}
                     <JAMButton value="Start learning" />
                 </JAMRow>
                 <JAMLoader />
+                <DataTable
+                    pagination
+                    highlightOnHover
+                    columns={columns}
+                    data={data}
+                />
             </JAMCol>
         </JAMPanel>
     )
